f7afe54dc3c3f024fe703a2225271e15ddb9f1b4
Nivekithan S <nivekithan363@gmail.com>
2022-08-11 16:40:03 +0530

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { getInput } from "@actions/core";
 
 try {
-  const githubToken = getInput("githubToken");
+  const githubToken = getInput("github-token");
   console.log(`github token is ${githubToken}`);
 } catch (err) {}
