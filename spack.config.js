9541a25e835cd052f5b5b3311d09f08c1882ec1f
Nivekithan S <nivekithan363@gmail.com>
2022-08-11 16:38:27 +0530

    fix it

--- spack.config.js
+++ spack.config.js
@@ -5,5 +5,6 @@ module.exports = config({
   entry: path.resolve(__dirname, "src", "index.ts"),
   target: "node",
   mode: "production",
+  options: { module: { type: "commonjs" } },
   output: { path: path.resolve(__dirname, "dist"), name: "index.js" },
 });
