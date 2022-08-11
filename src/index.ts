b25b80469e30e6a326d160c5195e80b8fd97f9b6
Nivekithan S <nivekithan363@gmail.com>
2022-08-11 16:43:05 +0530

    have i fixed it ?

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { getInput } from "@actions/core";
 
 try {
   const githubToken = getInput("github-token", { required: true });
-  console.log(`github token is ${githubToken}`);
-} catch (err) {}
+  console.log(`github token is ${githubToken} Tada`);
+} catch (err) {
+  console.log(err);
+}
