051c212f17f278f1b69b5b64f09fe47471a73ea6
Nivekithan S <nivekithan363@gmail.com>
2022-08-11 17:13:21 +0530

    check

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { getInput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 
 try {
   const githubToken = getInput("github-token", { required: true });
-  console.log(`github token is ${githubToken}`);
+  const octokit = getOctokit(githubToken);
+
+  const repoName = `${context.repo.owner}/${context.repo.repo}`;
+  const pushedBranch = `${context.ref}`;
+
+  console.log({ repoName, pushedBranch });
 } catch (err) {
   console.log(err);
 }
